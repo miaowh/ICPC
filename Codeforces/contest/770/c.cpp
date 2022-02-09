1d36c56cd3523104bbb9afd1c5ebd08ab31e5aa7
miao <576072415@qq.com>
2022-02-10 00:59:21 +0800

    1

--- Codeforces/contest/770/c.cpp
+++ Codeforces/contest/770/c.cpp
@@ -2,4 +2,23 @@
 using namespace std;
 using ll = long long;
 
-int main() {}
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, k;
+        cin >> n >> k;
+        if (k > 1 and (n & 1 or n * k % 2)) {
+            cout << "NO" << endl;
+        } else {
+            cout << "YES" << endl;
+            vector<int> res;
+            for (int i = 1; i <= n * k; i += 2) res.push_back(i);
+            for (int i = 2; i <= n * k; i += 2) res.push_back(i);
+            for (int i = 0; i < res.size(); i++) {
+                cout << res[i] << ' ';
+                if (i % k == k - 1) cout << endl;
+            }
+        }
+    }
+}
--- Codeforces/contest/770/d.cpp
+++ Codeforces/contest/770/d.cpp
@@ -2,4 +2,39 @@
 using namespace std;
 using ll = long long;
 
-int main() {}
+int ask(int a, int b, int c) {
+    cout << "? " << a << ' ' << b << ' ' << c << endl;
+    int ret;
+    cin >> ret;
+    return ret;
+}
+void guess(int a, int b) { cout << "! " << a << ' ' << b << endl; }
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+
+        int mx = -1, idx = -1;
+        for (int i = 3; i <= n; i++) {
+            int res = ask(1, 2, i);
+            if (res > mx) {
+                mx = res;
+                idx = i;
+            }
+        }
+        int a = idx;
+        mx = -1, idx = -1;
+        for (int i = 2; i <= n; i++) {
+            if (i == a) continue;
+            int res = ask(1, a, i);
+            if (res > mx) {
+                mx = res;
+                idx = i;
+            }
+        }
+        guess(a, idx);
+    }
+}
