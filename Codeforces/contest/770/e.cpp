ea296f7edf1f6aa62354269f6f81e589b440e948
miao <576072415@qq.com>
2022-02-26 18:42:23 +0800

    cf

--- Codeforces/contest/770/e.cpp
+++ Codeforces/contest/770/e.cpp
@@ -1,147 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int maxn = 1e6 + 6;
+const int MAXN = 1e6 + 6, MAXM = 1e6 + 6;
 const int INF = 0x3f3f3f3f;
-struct Edge {
-    int u, v;
-    int cap, flow;
-    Edge(int u_, int v_, int cap_, int flow_)
-        : u(u_), v(v_), cap(cap_), flow(flow_) {}
-};
+using pii = pair<int, int>;
+vector<pii> G[MAXN];
+bool Vis[MAXM];
+vector<int> Ans;
+int EdgeId, CurArc[MAXN];
 
-struct Dinic {
-    int n, m, s, t;  //结点数，边数（包括反向弧），源点编号和汇点编号
-    vector<Edge> edges;  //边表。edge[e]和edge[e^1]互为反向弧
-    vector<int> G[maxn];  //邻接表，G[i][j]表示节点i的第j条边在e数组中的序号
-    bool vis[maxn];  // BFS使用
-    int d[maxn];     //从起点到i的距离
-    int cur[maxn];   //当前弧下标
-    void init(int n) {
-        this->n = n;
-        for (int i = 0; i <= n; i++) G[i].clear();
-        edges.clear();
+void addEdge(int u, int v) {
+    G[u].push_back({v, EdgeId});
+    G[v].push_back({u, EdgeId++});
+}
+void findEulerCircuit(int u) {
+    for (int &i = CurArc[u]; i < G[u].size(); i++) {
+        const auto &[v, id] = G[u][i];
+        if (Vis[id]) continue;
+        Vis[id] = true;
+        findEulerCircuit(v);
     }
-    int AddEdge(int from, int to, int cap) {
-        // cerr << from << "  " << to << " " << cap << endl;
-        edges.emplace_back(from, to, cap, 0);
-        edges.emplace_back(to, from, 0, 0);
-        m = edges.size();
-        G[from].push_back(m - 2);
-        G[to].push_back(m - 1);
-        return m - 2;
-    }
-    bool BFS() {
-        for (int i = 0; i <= n; ++i) vis[i] = false, d[i] = 0;
-        queue<int> q;
-        q.push(s);
-        d[s] = 0;
-        vis[s] = 1;
-        while (!q.empty()) {
-            int x = q.front();
-            q.pop();
-            for (int i = 0, v; i < G[x].size(); i++) {
-                Edge &e = edges[G[x][i]];
-                v = e.v;
-                if (!vis[v] && e.cap > e.flow) {
-                    vis[v] = 1;
-                    d[v] = d[x] + 1;
-                    q.push(v);
-                }
-            }
-        }
-        return vis[t];
-    }
-    int DFS(int x, int a) {
-        if (x == t || a == 0) return a;
-        int flow = 0, f;
-        for (int &i = cur[x], v; i < G[x].size(); i++) {  //从上次考虑的弧
-            Edge &e = edges[G[x][i]];
-            v = e.v;
-            if (d[x] + 1 == d[v] && (f = DFS(v, min(a, e.cap - e.flow))) > 0) {
-                e.flow += f;
-                edges[G[x][i] ^ 1].flow -= f;
-                flow += f;
-                a -= f;
-                if (a == 0) break;
-            }
-        }
-        return flow;
-    }
-    int Maxflow(int s, int t) {
-        this->s = s, this->t = t;
-        int flow = 0;
-        while (BFS()) {
-            for (int i = 0; i <= n; i++) cur[i] = 0;
-            flow += DFS(s, INF);
-        }
-        return flow;
-    }
-} dinic;
+    Ans.push_back(u);
+}
 
 bool work() {
     int m;
     scanf("%d", &m);
     vector<int> num(m);
     vector<vector<int>> vec(m);
-    map<int, int> id, cnt;
-    vector<vector<char>> ans(m);
+    map<int, int> id, rid, cnt;
+    map<int, int> ans[m];
 
-    int idNum = 0;
+    int idNum = m;
     for (int k = 0; k < m; k++) {
         scanf("%d", &num[k]);
-        ans[k].resize(num[k]);
         vec[k].resize(num[k]);
         for (int i = 0, u; i < num[k]; i++) {
             scanf("%d", &vec[k][i]);
             cnt[vec[k][i]]++;
-            if (!id[vec[k][i]]) id[vec[k][i]] = ++idNum;
+            if (!id[vec[k][i]]) {
+                rid[idNum] = vec[k][i];
+                id[vec[k][i]] = idNum++;
+            }
         }
     }
-    int sum = accumulate(num.begin(), num.end(), 0);
+    for (auto &u : cnt)
+        if (u.second & 1) return false;
 
-    dinic.init(sum + m + idNum + 1);
-
-    int fcnt = 0;
-    map<int, int> rid;
     for (int i = 0; i < m; i++) {
-        if (num[i] & 1) return false;
-        dinic.AddEdge(0, i + 1, num[i] / 2);
-
-        for (int j = 0; j < num[i]; j++) {
-            dinic.AddEdge(i + 1, m + 1 + fcnt, 1);
-            dinic.AddEdge(m + 1 + fcnt, sum + m + id[vec[i][j]], 1);
-            rid[m + 1 + fcnt] = j;
-            fcnt++;
+        for (auto &u : vec[i]) {
+            addEdge(i, id[u]);
         }
     }
 
-    int mxFlow = 0;
-    for (auto &[u, i] : id) {
-        if (cnt[u] & 1) return false;
-        dinic.AddEdge(sum + m + i, sum + m + idNum + 1, cnt[u] / 2);
-        mxFlow += cnt[u];
+    for (int i = 0; i < m; i++) {
+        if (CurArc[i] >= G[i].size()) continue;
+        findEulerCircuit(i);
+        for (int j = 0; j < Ans.size() - 1; j++) {
+            int u = Ans[j], v = Ans[j + 1];
+            if (u < m) ans[u][rid[v]]++;
+        }
+        Ans.clear();
     }
-
-    int flow = dinic.Maxflow(0, sum + m + idNum + 1);
-
-    if (flow * 2 != mxFlow) return false;
 
     puts("YES");
-    for (auto &e : dinic.edges) {
-        if (e.u >= 1 and e.u <= m and e.v >= m + 1 and e.v <= sum + m and
-            e.flow == 1) {
-            int i = e.u - 1;
-            int j = rid[e.v];
-            ans[i][j] = 'L';
-        }
-    }
     for (int i = 0; i < m; i++) {
-        for (int j = 0; j < num[i]; j++)
-            if (ans[i][j] == 'L')
+        for (auto &u : vec[i]) {
+            if (ans[i][u]) {
                 printf("L");
-            else
+                ans[i][u]--;
+            } else
                 printf("R");
+        }
         puts("");
     }
     return true;
@@ -152,14 +82,14 @@ int main() {
 }
 
 /*
-1, 1, 1, 2
-3, 3, 3, 4
-1, 3
 
-
-1, 1, 3, 2
-1, 1
-3, 2
+3
+4
+1 1 3 2
+2
+1 1
+2
+3 2
 
 average in overall
 average in local
--- test.cpp
+++ test.cpp
@@ -1,71 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1e6;
 
-struct DLX {
-    int L[maxn], R[maxn], U[maxn], D[maxn];
-    int col[maxn], row[maxn];
-    int sz[maxn], rowPtr[maxn], idx;
-    int maxDep, stk[maxn];
-    int ans;
-
-    void remove(int c) {
-        // remove the c-th column and corresponding rows
-        L[R[c]] = L[c];
-        R[L[c]] = R[c];
-        for (int i = D[c]; i != c; i = D[i])
-            for (int j = R[i]; j != i; j = R[j])
-                U[D[j]] = U[j], D[U[j]] = D[j], --sz[col[j]];
+const int MAXN = 105;
+bool vis[MAXN][MAXN];
+int dr[] = {-1, 0, 0, 1}, dc[] = {0, -1, 1, 0};
+int ans = 0;
+void dfs(const vector<vector<int>> &Board, int r, int c, int val, int pos) {
+    vis[r][c] = true;
+    val = val * 10 + Board[r][c];
+    if (pos == 3) {
+        ans = max(ans, val);
+        vis[r][c] = false;
+        return;
     }
-
-    void recover(int c) {
-        // recover the c-th column and corresponding rows
-        for (int i = U[c]; i != c; i = U[i])
-            for (int j = L[i]; j != i; j = L[j])
-                U[D[j]] = D[U[j]] = j, ++sz[col[j]];
-        L[R[c]] = R[L[c]] = c;
-    }
-
-    void build(int m) {
-        // 0 is a head node. column counts from 1. row can count from 0.
-        for (int i = 0; i <= m; i++) {
-            L[i] = i - 1, R[i] = i + 1;
-            U[i] = D[i] = i;
-        }
-        L[0] = m, R[m] = 0, idx = m;
-        memset(sz, 0, sizeof(sz));
-        memset(rowPtr, 0, sizeof(rowPtr));
-    }
-
-    void insert(int r, int c) {
-        // insert an element in r-th row, c-th column
-        row[++idx] = r, col[idx] = c, ++sz[c];
-        U[idx] = c, D[idx] = D[c], U[D[c]] = idx, D[c] = idx;
-        if (!rowPtr[r])
-            rowPtr[r] = L[idx] = R[idx] = idx;
-        else {
-            int x = rowPtr[r];
-            L[idx] = x, R[idx] = R[x];
-            L[R[x]] = idx, R[x] = idx;
+    int R = Board.size(), C = Board[0].size();
+    for (int i = 0; i < 4; i++) {
+        int nr = r + dr[i], nc = c + dc[i];
+        if (nr < R && nc < C && nr >= 0 && nc >= 0 && vis[nr][nc] == false) {
+            dfs(Board, nr, nc, val, pos + 1);
         }
     }
-
-    bool dance(int dep) {
-        if (!R[0]) {
-            maxDep = dep;
-            return true;
+    vis[r][c] = false;
+}
+int solution(vector<vector<int>> &Board) {
+    for (int i = 0; i < Board.size(); i++) {
+        for (int j = 0; j < Board[i].size(); j++) {
+            dfs(Board, i, j, 0, 0);
         }
-        int c = R[0];
-        for (int i = R[0]; i; i = R[i])
-            if (sz[i] < sz[c]) c = i;
-        remove(c);
-        for (int i = D[c]; i != c; i = D[i]) {
-            stk[dep] = row[i];
-            for (int j = R[i]; j != i; j = R[j]) remove(col[j]);
-            if (dance(dep + 1)) return true;
-            for (int j = L[i]; j != i; j = L[j]) recover(col[j]);
-        }
-        recover(c);
-        return false;
     }
-} dlx;
+    return ans;
+}
+int main() {
+    // vector<vector<int>> Board = {};
+    // vector<vector<int>> Board = {{1, 1, 1}, {1, 3, 4}, {1, 4, 3}};
+    // vector<vector<int>> Board = {{0, 1, 5, 0, 0}};
+    vector<vector<int>> Board = {
+        {9, 1, 1, 0, 7}, {1, 0, 2, 1, 0}, {1, 9, 1, 1, 0}};
+
+    cout << solution(Board) << endl;
+}
